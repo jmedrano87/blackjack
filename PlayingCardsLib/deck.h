690082b670a836a46eb265ade4dd5251db30b170
Jose Medrano <jmedrano87@users.noreply.github.com>
2016-03-21 22:50:24 -0700

    Added xml documentation to deck.h

--- PlayingCardsLib/deck.h
+++ PlayingCardsLib/deck.h
@@ -3,21 +3,32 @@
 
 #include "card.h"
 
-const unsigned deckSize = 52;	//No Jokers
+const unsigned deckSize = 52;
 
 class PLAYINGCARDSDLL_API Deck
 {
 public:
 	Deck();
-	void shuffle();							//Randomize order of cards and call resetDrawn when done
-	void print(std::ostream *out) const;	//Output rank and suit of each element of cards[]
-	Card drawCard();						//Returns a random Card and sets it drawn value to true
+	/// <summary>Randomize order of cards and call resetDrawn when done.</summary>
+	void shuffle();
+	/// <summary>Output rank and suit of each element of deck.cards[] to *out.</summary>
+	void print(std::ostream *out) const;
+	/// <summary>Returns a random Card object and sets its drawn value to true.</summary>
+	Card drawCard();
 
 private:
+	/// <summary>
+	/// The deck, represented as an array of Card objects.
+	/// </summary>
 	Card cards[deckSize];
-	void buildDeck();						//Creates the card objects and adds them to cards[]
+	/// <summary>Creates the card objects and adds them to deck.cards[]</summary>
+	void buildDeck();
+	/// <summary>
+	/// Returns a single Capital letter, as a string, to represent the card's suit.
+	/// </summary>
 	std::string determineSuit(unsigned) const;
-	void resetDrawn();						//Sets value of drawn to false for all card elements
+	/// <summary>Sets the value of card.drawn to false for all card[] elements.</summary>
+	void resetDrawn();
 
 };
 
