fe27e402709408eaaf41cadb42af812667a6079f
Jose Medrano <jmedrano87@users.noreply.github.com>
2016-03-21 21:17:24 -0700

    Added some xml documentation to player.h.

--- PlayingCardsLib/player.h
+++ PlayingCardsLib/player.h
@@ -4,12 +4,34 @@
 #include "card.h"
 #include <vector>
 
+/// <summary>
+/// An object that represents a standard card game player.
+/// </summary>
 class PLAYINGCARDSDLL_API Player
 {
 public:
+	/// <summary>
+	/// Default constructer. Sets <paramref name="wins"/>,
+	/// <paramref name="losses"/>, and <paramref name="no"/> to 0.
+	/// </summary>
 	Player();
+	/// <summary>
+	/// Adds the specified Card to this players <paramref name="hand"/>
+	/// </summary>
+	/// <param name='card'>The Card object to add to
+	/// <paramref name="hand"/>
+	/// </param>
 	void addToHand(Card);
+	/// <summary>
+	/// Returns the number of cards in this player's hand as
+	/// an uint.
+	/// </summary>
+	/// <returns>An unsigned interger.</returns>
 	unsigned getNoOfCards() const;
+	/// <summary>
+	/// Returns the Card object at this.hand[card].
+	/// </summary>
+	/// <param name='card'> The index of the Card object to return</param>
 	Card getCard(unsigned) const;
 	void discardHand();		//clear() hand
 	unsigned getNo();		//get the player number
